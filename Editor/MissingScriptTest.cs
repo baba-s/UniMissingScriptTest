87cc282eb1d1f4818437c5f0470d43bf8ff65c85
baba_s <shotababa1206@gmail.com>
2020-09-25 21:23:09 +0900

    first commit

---
+++ Editor/MissingScriptTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class MissingScriptTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Missing_Script_が存在しないか()
+		{
+			AllGameObjectTester.Test( gameObject => GameObjectUtility.GetMonoBehavioursWithMissingScriptCount( gameObject ) <= 0 );
+		}
+	}
+}
